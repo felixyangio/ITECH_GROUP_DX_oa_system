6e988d2b087f3181545ec32576ec5757522f7f6a
“FanYang” <1597634338@qq.com>
2026-03-17 08:16:34 +0000

    update settings

--- oaback/oa/settings.py
+++ oaback/oa/settings.py
@@ -132,7 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (user-uploaded)
 MEDIA_URL = '/media/'
